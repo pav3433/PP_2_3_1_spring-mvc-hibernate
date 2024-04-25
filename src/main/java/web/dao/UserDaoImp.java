321a3c9725ee0ce51b0a7ae00e6b1a78af21da7a
pav3433 <gubkin.14@yandex.ru>
2024-04-25 18:57:50 +0300

    change2

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -5,16 +5,13 @@ import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
+    @PersistenceContext
     private EntityManager entityManager;
-
-    @Autowired
-    public UserDaoImp(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
 
     @Override
     public List<User> getAllUsers() {
